c92a5081392627ca634a9d5bc98948eaf259db68
Anish <anishjhacse168@gmail.com>
2026-03-01 19:30:11 +0530

    User schema completed

---
+++ src/models/user.js
@@ -0,0 +1,46 @@
+const { mongoose } = require("mongoose");
+const { Schema } = mongoose;
+
+const userSchema = new Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+      minLength: 2,
+      maxLength: 10,
+    },
+    lastName: {
+      type: String,
+      required: true,
+      minLength: 3,
+      maxLength: 10,
+    },
+    emailID: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      lowercase: true,
+      immutable: true,
+    },
+    age: {
+      type: Number,
+      min: 10,
+      max: 80,
+    },
+    role: {
+      type: String,
+      enum: ["user", "admin"],
+      default: "user",
+    },
+    problemSolved: {
+      type: [String],
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+const User = mongoose.model("user", userSchema);
+module.exports = User;
