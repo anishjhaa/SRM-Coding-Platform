15df800bc2e638c350d91890c20d36d28d5822d7
Anish <anishjhacse168@gmail.com>
2026-03-19 21:34:26 +0530

    day6

--- src/routes/problemCreator.js
+++ src/routes/problemCreator.js
@@ -6,22 +6,30 @@ const {
   createProblem,
   updateProblem,
   deleteProblem,
-  getProblemById,
-  getAllProblem,
+  getProblemByID,
+  getAllProblems,
 } = require("../controllers/userProblem");
 const userMiddleware = require("../middleware/userMiddleware");
+
+console.log({
+  createProblem,
+  updateProblem,
+  deleteProblem,
+  getProblemByID,
+  getAllProblems,
+});
 
 // Create the Problem
 problemRouter.post("/create", adminMiddleware, createProblem);
 problemRouter.put("/update/:id", adminMiddleware, updateProblem);
 problemRouter.delete("/delete/:id", adminMiddleware, deleteProblem);
 
-problemRouter.get("/problemById/:id", userMiddleware, getProblemById);
-problemRouter.get("/getAllProblem", userMiddleware, getAllProblem);
-problemRouter.get(
-  "/problemSolvedByUser",
-  userMiddleware,
-  solvedAllProblembyUser,
-);
+problemRouter.get("/problemById/:id", userMiddleware, getProblemByID);
+problemRouter.get("/getAllProblem", userMiddleware, getAllProblems);
+// problemRouter.get(
+//   "/problemSolvedByUser",
+//   userMiddleware,
+//   solvedAllProblembyUser,
+// );
 
 module.exports = problemRouter;
