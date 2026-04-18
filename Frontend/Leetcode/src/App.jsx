9f3cc82dc0bb9a9d50f52afe5609d2fdf0b36482
Anish <anishjhacse168@gmail.com>
2026-04-19 01:39:08 +0530

    front

--- Frontend/Leetcode/src/App.jsx
+++ Frontend/Leetcode/src/App.jsx
@@ -14,27 +14,140 @@
 //   );
 // }
 // export default App;
-import { Routes, Route } from "react-router";
+// import { Routes, Route } from "react-router";
+// import Login from "./pages/Login";
+// import Signup from "./pages/Signup";
+// import Homepage from "./pages/Homepage";
+// import { checkAuth } from "./authSlice";
+// import { useDispatch, useSelector } from "react-redux";
+// import { useEffect } from "react";
+// function App() {
+//   const { isAuthenticated } = useSelector((state) => state.auth);
+//   const dispatch = useDispatch();
+
+//   useEffect(() => {
+//     dispatch(checkAuth());
+//   }, [isAuthenticated]);
+
+//   return (
+//     <>
+//       <Routes>
+//         <Route path="/" element={<Homepage></Homepage>}></Route>
+//         <Route path="/login" element={<Login></Login>}></Route>
+//         <Route path="/signup" element={<Signup></Signup>}></Route>
+//       </Routes>
+//     </>
+//   );
+// }
+// export default App;
+// import { Routes, Route, Navigate } from "react-router";
+// import Login from "./pages/Login";
+// import Signup from "./pages/Signup";
+// import Homepage from "./pages/Homepage";
+// import { checkAuth } from "./authSlice";
+// import { useDispatch, useSelector } from "react-redux";
+// import { useEffect } from "react";
+// import { checkAuth } from "./authSlice";
+// import AdminPanel from "./pages/AdminPanel";
+
+// function App() {
+//   const { isAuthenticated, user, loading } = useSelector((state) => state.auth);
+//   const dispatch = useDispatch();
+
+//   useEffect(() => {
+//     dispatch(checkAuth());
+//   }, [dispatch]);
+
+//   if (loading) {
+//     return (
+//       <div className="min-h-screen flex items-center justify-center">
+//         <span className="loading loading-spinner loading-lg"></span>
+//       </div>
+//     );
+//   }
+
+//   return (
+//     <>
+//       <Routes>
+//         <Route
+//           path="/"
+//           element={
+//             isAuthenticated ? <Homepage></Homepage> : <Navigate to="/signup" />
+//           }
+//         ></Route>
+//         <Route
+//           path="/login"
+//           element={isAuthenticated ? <Navigate to="/" /> : <Login></Login>}
+//         ></Route>
+//         <Route
+//           path="/signup"
+//           element={isAuthenticated ? <Navigate to="/" /> : <Signup></Signup>}
+//         ></Route>
+//         <Route path="/admin" element={<AdminPanel />}></Route>
+//         {/* <Route
+//         path="/admin"
+//         element={
+//           isAuthenticated && user?.role === 'admin' ?
+//             <AdminPanel /> :
+//             <Navigate to="/" />
+//         }
+//       /> */}
+//       </Routes>
+//     </>
+//   );
+// }
+// export default App;
+import { Routes, Route, Navigate } from "react-router";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import Homepage from "./pages/Homepage";
 import { checkAuth } from "./authSlice";
 import { useDispatch, useSelector } from "react-redux";
 import { useEffect } from "react";
+import AdminPanel from "./pages/AdminPanel";
+
 function App() {
-  const { isAuthenticated } = useSelector((state) => state.auth);
+  const { isAuthenticated, user, loading } = useSelector((state) => state.auth);
   const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch(checkAuth());
-  }, [isAuthenticated]);
+  }, [dispatch]);
+
+  if (loading) {
+    return (
+      <div className="min-h-screen flex items-center justify-center">
+        <span className="loading loading-spinner loading-lg"></span>
+      </div>
+    );
+  }
 
   return (
     <>
       <Routes>
-        <Route path="/" element={<Homepage></Homepage>}></Route>
-        <Route path="/login" element={<Login></Login>}></Route>
-        <Route path="/signup" element={<Signup></Signup>}></Route>
+        <Route
+          path="/"
+          element={
+            isAuthenticated ? <Homepage></Homepage> : <Navigate to="/signup" />
+          }
+        ></Route>
+        <Route
+          path="/login"
+          element={isAuthenticated ? <Navigate to="/" /> : <Login></Login>}
+        ></Route>
+        <Route
+          path="/signup"
+          element={isAuthenticated ? <Navigate to="/" /> : <Signup></Signup>}
+        ></Route>
+        <Route path="/admin" element={<AdminPanel />}></Route>
+        {/* <Route 
+        path="/admin" 
+        element={
+          isAuthenticated && user?.role === 'admin' ? 
+            <AdminPanel /> : 
+            <Navigate to="/" />
+        } 
+      /> */}
       </Routes>
     </>
   );
